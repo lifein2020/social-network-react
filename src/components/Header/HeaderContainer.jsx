5a3a6e11f492a79137b44c032ef0e491048b13af
lifein2020 <lady.zhivtsova@gmail.com>
2022-12-19 11:40:09 +0300

    67-lesson_redux-thunk2

--- src/components/Header/HeaderContainer.jsx
+++ src/components/Header/HeaderContainer.jsx
@@ -1,24 +1,12 @@
 import React from 'react';
 import Header from './Header';
 import { connect } from 'react-redux';
-import {setAuthUserData} from '../../redux/auth-reducer';
-import { authAPI } from '../../api/api';
+import { getAuthUserData } from '../../redux/auth-reducer';
+
 
 class HeaderContainer extends React.Component {
     componentDidMount() {
-        // this.props.toggleIsFetching(true);
-        authAPI.me()
-        // перенесли в api.js
-        // axios.get(`https://social-network.samuraijs.com/api/1.0/auth/me`, { // запрос чтобы понять кто мы, залогинены или нет
-        //     withCredentials: true // этот параметр get запроса - настройки запроса. Если пользователь был ранее залогинен на сайте https://social-network.samuraijs.com, то информация об этом сохранилась в куки того браузера, через который логинился. При запросе на сервер отправляется кука с информацией о логине.
-        // })
-            .then(response => {
-       //debugger // смотрим в F12 что в response.data.message, response.data.resultCode
-                if (response.data.resultCode === 0) {
-                    let { id, email, login } = response.data.data; // деструктуризация
-                    this.props.setAuthUserData(id, email, login ); // проверить что пришло F12 -> Console -> store.getState().auth
-                }
-            });
+        this.props.getAuthUserData(); //намерение получить данные
     }
 
     render() {
@@ -31,4 +19,4 @@ const mapStateToProps = (state) => ({
     login: state.auth.login,
 });
 
-export default connect (mapStateToProps, {setAuthUserData} ) (HeaderContainer);
+export default connect (mapStateToProps, {getAuthUserData} ) (HeaderContainer);
